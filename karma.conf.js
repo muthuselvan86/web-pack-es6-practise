be6914e589f648d5d8b906a1d576548c8e2807a0
muthuselvan86 <muthuselvan86>
2018-06-27 23:38:08 -0400

    setup unit test case: karma + webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Wed Jun 27 2018 20:11:22 GMT-0400 (EDT)
 
+let webPackConfig = require("./webpack.config");
+
 module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -11,14 +13,18 @@ module.exports = function(config) {
     frameworks: ["jasmine"],
 
     // list of files / patterns to load in the browser
-    files: ["./src/**/{**,.}/*.js"],
+    files: ["./src/**/{**,.}/*.spec.js"],
 
     // list of files / patterns to exclude
     exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+      "./src/**/{**,.}/*.spec.js": ["webpack"]
+    },
+
+    webpack: webPackConfig,
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
---
+++ src/js/views/course-view.spec.js
@@ -0,0 +1,5 @@
+describe("Test setup", function() {
+  it("summing of values work", function() {
+    expect(2 + 3).toEqual(5);
+  });
+});
